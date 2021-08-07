c36e01bce8293b3c4c8a921d9ad454e5686579b3
tolaind <kqha0051@yandex.ru>
2021-08-07 17:43:09 +0700

    Thêm file bài tập in thông tin

---
+++ InThongTin.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#Bài tập in thông tin cá nhân
+print(" Họ và tên: Vũ Hoàng Anh\n Ngày sinh: 12/07/1990\n Địa chỉ: Quận 2, tp.HCM\n Mục đích đến với khoá học Python: sử dụng thành thạo ngôn ngữ lập trình python,\n  quy trình phát triển phần mềm và chia sẻ mã nguồn")
+
+
+# In[ ]:
+
+
+
+
