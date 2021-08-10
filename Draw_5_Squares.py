3c8f38f223f53f8c692a8f317d23a3aa8c899445
tolaind <kqha0051@yandex.ru>
2021-08-10 18:00:21 +0700

    Thêm bài vẽ 5 hình vuông

---
+++ Draw_5_Squares.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import turtle
+t = turtle.Turtle()
+t.pensize(2)
+count = 0
+while count < 5:
+    t.forward(100)
+    t.right(90)
+    t.forward(200)
+    t.right(90)
+    t.forward(200)
+    t.right(90)
+    t.forward(200)
+    t.right(90)
+    t.forward(100)
+    t.right(72)
+    count += 1 
+
+
+
+turtle.done()
+turtle.mainloop()
+
+
+# In[ ]:
+
+
+
+
